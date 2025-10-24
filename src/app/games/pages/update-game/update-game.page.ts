a6b39d02b354896ac6bbe980dc7a3b2faa9bef66
neumann.niklas <neumann.niklas98@gmail.com>
2025-10-25 00:52:39 +0200

    complex updateGame getter function fix

--- src/app/games/pages/update-game/update-game.page.ts
+++ src/app/games/pages/update-game/update-game.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { forkJoin } from 'rxjs';
 import { AuthStoreService } from '../../../auth/services/auth-store.service';
+import { Category } from '../../../categories/models/category.model';
+import { CategoryStoreService } from '../../../categories/services/category-store.service';
 import { Game, UpdateGame } from '../../models/game.model';
 import { GroupPhase, groupPhases } from '../../models/group-phase.enum';
 import { GameStoreService } from '../../services/game-store.service';
@@ -15,41 +18,74 @@ import { GameStoreService } from '../../services/game-store.service';
 export class UpdateGamePage implements OnInit {
   readonly updateGameFormGroup: FormGroup = new FormGroup({
     title: new FormControl<string | null>(null),
-    groupPhase: new FormControl<GroupPhase>(GroupPhase.Forming)
+    groupPhase: new FormControl<GroupPhase>(GroupPhase.Forming),
+    categories: new FormControl<Category[]>([])
   });
   readonly groupPhases: { key: string, value: string | GroupPhase }[] = groupPhases;
 
   readonly id: string | null;
+
   readonly isAuthenticated: Signal<boolean>;
+  readonly categories: Signal<Category[]>;
   readonly game: Signal<Game | null>;
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly router: Router,
     private readonly authStoreService: AuthStoreService,
+    private readonly categoriesStoreService: CategoryStoreService,
     private readonly gameStoreService: GameStoreService
   ) {
-    this.id = this.activatedRoute.snapshot.paramMap.get('id') ?? null;
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+
     this.isAuthenticated = this.authStoreService.isAuthenticated;
+    this.categories = this.categoriesStoreService.categories;
     this.game = this.gameStoreService.game;
   }
 
   ngOnInit(): void {
     if (!this.id) return;
 
-    this.gameStoreService.getGame(this.id).subscribe({
-      next: (game: Game) => this.updateGameFormGroup.patchValue({
-        groupPhase: game.groupPhase
+    forkJoin({
+      categories: this.categoriesStoreService.getCategories(),
+      game: this.gameStoreService.getGame(this.id)
+    }).subscribe({
+      next: ({ categories, game }: { categories: Category[], game: Game }) => this.updateGameFormGroup.patchValue({
+        groupPhase: game.groupPhase,
+        categories: this.mapCategories(game.categories, categories)
       })
     });
+  }
+
+  private mapCategories(gameCategories: Category[], categories: Category[]): Category[] {
+    return gameCategories.map((gameCategory: Category) => categories.find((category: Category) => category.id === gameCategory.id))
+      .filter((category: Category | undefined) => !!category);
+  }
+
+  private cleanFormValues(values: any): any {
+    return Object.fromEntries(Object.entries(values).filter(([_, value]) => value !== null && value !== ''));
+  }
+
+  private areCategoriesEqual(a: Category[], b: Category[]): boolean {
+    if (a.length !== b.length) return false;
+
+    return a.map((category: Category) => category.id).sort().every((id, i) => id === b.map((category: Category) => category.id).sort()[i]);
   }
 
   get updateGame(): UpdateGame {
     const game = this.game();
 
-    if (!game) return Object.fromEntries(Object.entries(this.updateGameFormGroup.value).filter(([_, value]) => value !== null && value !== ''));
+    if (!game) return this.cleanFormValues(this.updateGameFormGroup.value);
 
-    return Object.fromEntries(Object.entries(this.updateGameFormGroup.value).filter(([key, value]) => value !== null && value !== '' && game[key as keyof Game] !== value));
+    const updateGame: any = {};
+
+    for (const [key, value] of Object.entries(this.cleanFormValues(this.updateGameFormGroup.value))) {
+      if (key === 'categories') {
+        if (!this.areCategoriesEqual(value as Category[], game.categories)) updateGame.categories = value;
+      } else if (game[key as keyof Game] !== value) updateGame[key] = value;
+    }
+
+    return updateGame as UpdateGame;
   }
 
   get isUpdateGameValid(): boolean {
@@ -58,7 +94,8 @@ export class UpdateGamePage implements OnInit {
 
   onReset(): void {
     this.updateGameFormGroup.reset({
-      groupPhase: this.game()?.groupPhase
+      groupPhase: this.game()?.groupPhase,
+      categories: this.mapCategories(this.game()?.categories ?? [], this.categories())
     });
   }
 
@@ -66,7 +103,7 @@ export class UpdateGamePage implements OnInit {
     if (!this.id) return;
 
     this.gameStoreService.updateGame(this.id, this.updateGame).subscribe({
-      complete: () => this.router.navigate(['/'])
+      complete: () => this.router.navigate([''])
     });
   }
 }
