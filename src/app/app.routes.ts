60f33921824dc69f7c05e0503d024ba729b2e551
neumann.niklas <neumann.niklas98@gmail.com>
2025-10-30 00:51:33 +0100

    initial categories feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { authenticationGuard } from './core/guards/authentication-guard';
 
 export const routes: Routes = [
     {
+        path: 'categories',
+        loadComponent: () => import('./features/categories/pages/categories/categories.page').then(m => m.CategoriesPage)
+    },
+    {
         path: 'user',
         loadComponent: () => import('./features/auth/pages/user/user.page').then(m => m.UserPage),
         canActivate: [authenticationGuard]
