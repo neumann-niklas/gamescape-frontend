60da8157834cd4512cbffa8854ff135e06056d5e
neumann.niklas <neumann.niklas98@gmail.com>
2025-10-30 19:47:23 +0100

    GameGridComponent

--- src/app/features/games/components/game-grid/game-grid.component.ts
+++ src/app/features/games/components/game-grid/game-grid.component.ts
@@ -1,9 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, input, InputSignal } from '@angular/core';
+import { GroupPhasePipe } from '../../../../core/pipes/group-phase-pipe';
 import { Game } from '../../models/game.model';
 
 @Component({
   selector: 'app-game-grid',
-  imports: [],
+  imports: [DatePipe, GroupPhasePipe],
   templateUrl: './game-grid.component.html',
   styleUrl: './game-grid.component.scss'
 })
